cbff190e7244f0bd7d8dc1036daae5a8cf88d7d9
charanreddy262 <charan.madhi@gmail.com>
2017-06-25 20:52:08 -0500

    Update App.spec.js

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,13 +1,11 @@
-/**
- * Created by charan reddy on 6/22/2017.
- */
 import React from 'react';
-import App from '../src/App';
-
-describe('App test', () => {
-    const wrapper = shallow(<App />);
-
-    it('should  be a div', () => {
-        expect(wrapper.type()).to.eql('div');
-    });
-});
+describe('Testing the App Component',()=>{
+    let component;
+    beforeEach(()=>{
+        component=shallow(<Sample/>);
+    })
+    it('check component',()=>{
+        expect(component).to.exist;
+    })
+    it('check for ')
+})
