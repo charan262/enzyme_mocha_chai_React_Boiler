b09fc127020055a29984f5c6f52f0dbb60cd3fe6
charanreddy262 <charan.madhi@gmail.com>
2017-06-25 22:01:37 -0500

    Update dom.js

--- test/dom.js
+++ test/dom.js
@@ -1,9 +1,13 @@
 
 var jsdom = require('jsdom').jsdom;
 var exposedProperties = ['window', 'navigator', 'document'];
-
+import _$ from 'jquery';
+import chai from 'chai'
+import chaiJquery from 'chai-jquery'
 global.document = jsdom('');
 global.window = document.defaultView;
+const $ = _$(window);
+chaiJquery(chai, chai.util, $);
 Object.keys(document.defaultView).forEach((property) => {
     if (typeof global[property] === 'undefined') {
         exposedProperties.push(property);
